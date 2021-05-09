1540d2fb454ba1c2cf31af6bf6f55cd39726f5da
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2021-05-09 16:10:16 +0900

    Put link to home on Header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import Anchor from "../components/Anchor";
 
 export default function Header() {
   return (
     <Container>
+      <Overlay href="/" />
       <Title>科学の旅</Title>
       <SubTitle>理系大学生の数学・計算機科学の備忘録</SubTitle>
     </Container>
@@ -13,14 +15,25 @@ export default function Header() {
 const Container = styled.header`
   position: relative;
   height: 64px;
-  padding-left: 4px;
   display: flex;
   flex-direction: column;
   background-color: lightblue;
-
   text-align: center;
+
   @media (max-width: 600px) {
+    padding-left: 4px;
     text-align: left;
+  }
+`;
+
+const Overlay = styled(Anchor)`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  z-index: 1;
+
+  @media (max-width: 600px) {
+    width: calc(100% - 4px);
   }
 `;
 
