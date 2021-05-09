b0d3b55309805ef9e7e6734eecca5249dd6381fa
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2021-05-10 00:28:10 +0900

    Add Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,25 @@ export default class _Document extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          {process.env.NODE_ENV === "production" && (
+            <>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-FK8E6HH56L"></script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-FK8E6HH56L');
+              `,
+                }}
+              />
+            </>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
