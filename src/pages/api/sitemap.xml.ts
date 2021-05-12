04d98da542906070f901e05aa00e671561fbc3f7
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2021-05-12 10:35:48 +0900

    Add sitemap

---
+++ src/pages/api/sitemap.xml.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { noteMap } from "../../article/ArticleLoader";
+
+type SitemapUrl = { loc: string };
+
+class SitemapBuilder {
+  readonly urls: SitemapUrl[] = [];
+
+  add(url: SitemapUrl) {
+    this.urls.push(url);
+  }
+
+  build() {
+    return `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${this.urls.map((u) => `<url><loc>https://science-journey.net${u.loc}</loc></url>`.trim()).join("")}
+      </urlset>
+    `.trim();
+  }
+}
+
+export default async (_: NextApiRequest, res: NextApiResponse) => {
+  const builder = new SitemapBuilder();
+  noteMap.books.forEach((b) => {
+    builder.add({ loc: `/${b.name}` });
+    b.units.forEach((u) => {
+      u.sections.forEach((s) => {
+        builder.add({ loc: `/${b.name}/${u.name}/${s.name}` });
+        s.notes.forEach((n) => builder.add({ loc: `/${b.name}/${u.name}/${s.name}/${n.name}` }));
+      });
+    });
+  });
+
+  res.setHeader("Content-Type", "application/xml");
+  res.write(builder.build());
+  res.end();
+};
