33b2c1d0cb86683304de035637004e0510a6d399
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2021-05-13 10:45:54 +0900

    Add prebuild script to create sitemap.xml

---
+++ pre-scripts/prebuild.js
@@ -0,0 +1,42 @@
+/**
+ * prebuild script
+ *
+ * sitemap.xml を作成する
+ */
+
+const fs = require("fs");
+
+class SitemapBuilder {
+  urls = [];
+
+  add(url) {
+    this.urls.push(url);
+  }
+
+  build() {
+    return `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${this.urls.map((u) => `<url><loc>https://science-journey.net${u.loc}</loc></url>`.trim()).join("")}
+        </urlset>
+    `.trim();
+  }
+}
+
+const builder = new SitemapBuilder();
+
+const addHtmls = (dir) => {
+  fs.readdirSync(dir)
+    .filter((f) => !f.includes("_next") && (!f.includes(".") || f.endsWith(".html")) && f !== "404.html")
+    .map((f) => dir + "/" + f)
+    .forEach((f) => {
+      if (fs.lstatSync(f).isDirectory()) {
+        addHtmls(f);
+      } else {
+        builder.add({ loc: f.substr("./out".length) });
+      }
+    });
+};
+addHtmls("./out");
+
+fs.writeFileSync("./public/sitemap.xml", builder.build());
