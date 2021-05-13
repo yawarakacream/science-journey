82a91644e9364da18a0d597fd3cfbda4f34099b3
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2021-05-13 11:40:32 +0900

    Fix file extension

--- sj-scripts/sitemap-generator.js
+++ sj-scripts/sitemap-generator.js
@@ -15,35 +15,47 @@ class SitemapBuilder {
   }
 
   build() {
-    return `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${this.urls.map((u) => `<url><loc>https://science-journey.net${u.loc}</loc></url>`).join("")}
-        </urlset>
-    `.replace(/(\s|\n)/g, "");
+    return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${this.urls.map((u) => `<url><loc>https://science-journey.net${u.loc}</loc></url>`).join("")}
+</urlset>`.replace(/\n/g, "");
   }
 }
 
 const builder = new SitemapBuilder();
 
-const addHtmls = (dir) => {
-  fs.readdirSync(dir)
-    .filter((f) => !f.includes("_next") && (!f.includes(".") || f.endsWith(".html")) && f !== "404.html")
-    .map((f) => dir + "/" + f)
-    .forEach((f) => {
-      if (fs.lstatSync(f).isDirectory()) {
-        addHtmls(f);
-      } else {
-        builder.add({ loc: f.substr("./out".length) });
-      }
-    });
+const createSitemap = () => {
+  const addHtmls = (dir) => {
+    fs.readdirSync(dir)
+      .filter((f) => !f.includes("_next") && (!f.includes(".") || f.endsWith(".html")) && f !== "404.html")
+      .map((f) => dir + "/" + f)
+      .forEach((f) => {
+        if (fs.lstatSync(f).isDirectory()) {
+          addHtmls(f);
+        } else {
+          builder.add({ loc: f.slice("./out".length, -".html".length) });
+        }
+      });
+  };
+
+  addHtmls("./out");
+
+  fs.writeFileSync("./public/sitemap.xml", builder.build());
 };
-addHtmls("./out");
 
-fs.writeFileSync("./public/sitemap.xml", builder.build());
+const commit = () => {
+  exec("git diff --name-only --cached", (_, stdout) => {
+    if (stdout.length) {
+      throw new Error("Staged files are uncommitted");
+    }
+    exec("git add ./public/sitemap.xml", () => {
+      exec("git commit -m 'Update sitemap.xml (sitemap-generator)'");
+    });
+  });
+};
 
-exec("git add ./public/sitemap.xml", (_, stdout) => {
-  if (stdout) {
-    exec("git commit -m 'Update sitemap.xml (sitemap-generator)'");
-  }
-});
+const main = () => {
+  createSitemap();
+  commit();
+};
+main();
